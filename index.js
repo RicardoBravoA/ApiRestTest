058eb3042ef46cbed497605da37abde7faf4de76
Ricardo Bravo <ricardobravo@outlook.com>
2021-02-04 18:51:02 -0500

    refactor: Update typo error

--- index.js
+++ index.js
@@ -4,8 +4,9 @@
 //mongoose module
 var mongoose = require('mongoose');
 
+mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/curso_mongo', {useNewUrlParser: true})
 		.then(() => {
-			console.log('Connection successfull');
+			console.log('Connection successful');
 		})
 		.catch(error => console.log(error));
