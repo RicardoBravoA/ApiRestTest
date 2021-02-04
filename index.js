418c56ae44626c245d6f901b002ad1b92d18729c
Ricardo Bravo <ricardobravo@outlook.com>
2021-02-04 18:43:53 -0500

    refactor: Remove deprecated sentence

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 //mongoose module
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/curso_mongo', {useMongoClient: true})
+mongoose.connect('mongodb://localhost:27017/curso_mongo', {useNewUrlParser: true})
 		.then(() => {
 			console.log('Connection successfull');
 		})
