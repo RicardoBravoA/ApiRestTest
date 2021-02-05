1a2a57f0498a79f7a7febe5d7f1c88457595db8a
Ricardo Bravo <ricardobravo@outlook.com>
2021-02-04 19:05:23 -0500

    feat: Add port

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 
 //mongoose module
 var mongoose = require('mongoose');
-var app = require('./app')
+var app = require('./app');
+var port = 3000;
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/curso_mongo', {
