db23754d4d33cf7ee72754c2afbf0222e9b91cf3
Ricardo Bravo <ricardobravo@outlook.com>
2021-02-04 23:24:56 -0500

    feat(video.route): Add route for save video

--- route/video.route.js
+++ route/video.route.js
@@ -6,5 +6,6 @@ var videoController = require('../controller/video.controller');
 var api = express.Router();
 
 api.get('/test', videoController.test);
+api.post('/video', videoController.save);
 
 module.exports = api;
