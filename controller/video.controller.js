9e14a911a41384f53a64bb88ce9cfc7c6f4a0567
Ricardo Bravo <ricardobravo@outlook.com>
2021-02-04 23:17:06 -0500

    feat(video.controller): Add validation for name field

--- controller/video.controller.js
+++ controller/video.controller.js
@@ -1,12 +1,26 @@
 'use strict'
 
-var video = require('../model/video');
+var videoModel = require('../model/video.model');
 
 function test(request, response) {
 	response.status(200).send({
 		message: 'Test from controller'
 	});
 };
+
+function save(request, response){
+	var video = new VideoModel();
+
+	var params = request.body();
+
+	if(!params.name){
+		response.status(500).send({
+			message: 'Name is required'
+		});
+	}
+
+
+}
 
 module.exports = {
 	test
