d33cdfcd2a6b0191ec48c3f86666c9aa02e38500
Ricardo Bravo <ricardobravo@outlook.com>
2021-02-04 22:55:11 -0500

    feat(video.route): Export module

--- route/video.route.js
+++ route/video.route.js
@@ -4,3 +4,7 @@ var express = require('express');
 var videoController = require('../controller/video.controller');
 
 var api = express.Router();
+
+api.get('/tests', videoController.test);
+
+module.exports = api;
