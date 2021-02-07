26c124bd4bb74b0148625c58e72654efc02ee0de
Ricardo Bravo <ricardobravo@outlook.com>
2021-02-06 23:31:26 -0500

    feat: Add constant class

--- contract/error.contract.js
+++ contract/error.contract.js
@@ -2,13 +2,13 @@
 
 var ErrorModel = require('../model/error.model');
 
-const SERVER_ERROR =  'Server error';
-
 function serverError(response) {
 	response.status(500).send({
 		message: SERVER_ERROR
 	});
 };
+
+
 
 module.exports = {
 	serverError
---
+++ util/constant.js
@@ -0,0 +1,7 @@
+const SERVER_ERROR =  'Server error';
+const VIDEO_NOT_FOUND =  'Video not found';
+
+module.exports = {
+  SERVER_ERROR,
+  VIDEO_NOT_FOUND
+}
