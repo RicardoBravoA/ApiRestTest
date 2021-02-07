5308b63eb2b9543b030ba14bbbf31cfcf6b5c823
Ricardo Bravo <ricardobravo@outlook.com>
2021-02-06 23:47:31 -0500

    feat(Constant): Add error saving video constant

--- util/constant.js
+++ util/constant.js
@@ -1,7 +1,9 @@
 const SERVER_ERROR =  'Server error'
 const VIDEO_NOT_FOUND =  'Video not found'
+const ERROR_SAVING_VIDEO =  'Error saving video'
 
 module.exports = {
   SERVER_ERROR,
-  VIDEO_NOT_FOUND
+  VIDEO_NOT_FOUND,
+  ERROR_SAVING_VIDEO
 }
