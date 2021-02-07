f1fd0a6f322862d48ac3f2f715a7384e46e0501e
Ricardo Bravo <ricardobravo@outlook.com>
2021-02-06 23:12:46 -0500

    feat: Export error model

---
+++ model/error.model.js
@@ -0,0 +1,11 @@
+'use strict'
+
+var mongoose = require('mongoose');
+
+var schema = mongoose.Schema;
+
+var errorSchema = schema({
+	message: String
+});
+
+module.exports = mongoose.model('Error', errorSchema);
