34e27741a175815ba95f8aac53cdd0b513989c3f
Ricardo Bravo <ricardobravo@outlook.com>
2021-02-07 00:01:54 -0500

    feat(constant): Add name is required constant

--- util/constant.js
+++ util/constant.js
@@ -1,9 +1,11 @@
 const SERVER_ERROR =  'Server error'
 const VIDEO_NOT_FOUND =  'Video not found'
 const ERROR_SAVING_VIDEO =  'Error saving video'
+const NAME_IS_REQUIRED =  'Name is required'
 
 module.exports = {
   SERVER_ERROR,
   VIDEO_NOT_FOUND,
-  ERROR_SAVING_VIDEO
+  ERROR_SAVING_VIDEO,
+  NAME_IS_REQUIRED
 }
