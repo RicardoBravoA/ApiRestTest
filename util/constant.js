82473d676c7e87cf05ea20a0cf30d655dcd48090
Ricardo Bravo <ricardobravo@outlook.com>
2021-02-07 00:02:29 -0500

    feat(constant): Add user is required constant

--- util/constant.js
+++ util/constant.js
@@ -2,10 +2,12 @@ const SERVER_ERROR =  'Server error'
 const VIDEO_NOT_FOUND =  'Video not found'
 const ERROR_SAVING_VIDEO =  'Error saving video'
 const NAME_IS_REQUIRED =  'Name is required'
+const USER_IS_REQUIRED =  'User is required'
 
 module.exports = {
   SERVER_ERROR,
   VIDEO_NOT_FOUND,
   ERROR_SAVING_VIDEO,
-  NAME_IS_REQUIRED
+  NAME_IS_REQUIRED,
+  USER_IS_REQUIRED
 }
