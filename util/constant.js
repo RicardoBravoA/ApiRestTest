8ee26f29a5b7bb3556903539f5ef666926e34060
Ricardo Bravo <ricardobravo@outlook.com>
2021-02-08 17:26:56 -0500

    feat(constant): Add http code

--- util/constant.js
+++ util/constant.js
@@ -3,6 +3,9 @@ const VIDEO_NOT_FOUND =  'Video not found'
 const ERROR_SAVING_VIDEO =  'Error saving video'
 const NAME_IS_REQUIRED =  'Name is required'
 const USER_IS_REQUIRED =  'User is required'
+const CODE_200 =  200
+const CODE_500 =  500
+
 
 module.exports = {
   SERVER_ERROR,
